053dba7d871b96b53f82a580d7d3cfc02937c38e
Erwin Macaraig <erwin.macaraig@gmail.com>
2025-01-08 17:22:13 +0800

    discovered the right component for getting user
    implement like and dislike with db operations in post

--- src/Twig/Components/LikeComponent.php
+++ src/Twig/Components/LikeComponent.php
@@ -8,13 +8,19 @@ use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Psr\Log\LoggerInterface;
 
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface; 
+use Symfony\Component\Security\Core\Security;
 
 #[AsLiveComponent]
 final class LikeComponent
 {
     use DefaultActionTrait;
 
-    public $post;
+    #[LiveProp(writable:true)]
+    public Post $post;
+    public $isLiked;
+    public $isDisliked;
 
     #[LiveProp(writable: true)]
     public $likes;
@@ -22,28 +28,49 @@ final class LikeComponent
     #[LiveProp(writable: true)]
     public $dislikes; 
 
+    private $entityManager;
+    private $security;
+
+    public function __construct(EntityManagerInterface $entityManager, Security $security)
+    {
+        $this->entityManager = $entityManager;
+        $this->security = $security;
+    }
+
     #[LiveAction]
     public function like(LoggerInterface $logger)
     {
-        $this->likes++;        
+        $this->post->addUsersThatLike($this->security->getUser());  
+        $this->isLiked = true;
+        $this->entityManager->persist($this->post);
+        $this->entityManager->flush();   
     }
 
     #[LiveAction] 
     public function undoLike()
     {
-        $this->likes--;
+        $this->post->removeUsersThatLike($this->security->getUser());  
+        $this->isLiked = false;
+        $this->entityManager->persist($this->post);
+        $this->entityManager->flush();   
     }
 
     #[LiveAction] 
     public function dislike() 
     {
-        $this->dislikes++;
+        $this->post->addUsersThatDontLike($this->security->getUser());  
+        $this->isDisliked = true;
+        $this->entityManager->persist($this->post);
+        $this->entityManager->flush();   
     }
 
     #[LiveAction] 
     public function undoDislike() 
     {
-        $this->dislikes--;
+        $this->post->removeUsersThatDontLike($this->security->getUser());  
+        $this->isDisliked = false;
+        $this->entityManager->persist($this->post);
+        $this->entityManager->flush();   
     }
 
     public function getRandomNumber(): int
--- src/Twig/Components/SearchComponent.php
+++ src/Twig/Components/SearchComponent.php
@@ -4,9 +4,18 @@ namespace App\Twig\Components;
 
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
 
 #[AsLiveComponent()]
 final class SearchComponent
 {
     use DefaultActionTrait;
+
+    #[LiveProp(writable: true)]
+    public string $query = '';
+
+    public function getPosts(): array 
+    {
+        return $this->query ? [['title' => $this->query]]: [];
+    }
 }
